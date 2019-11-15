d0485b85fa10a99a8055bee196443f5ff84677cf
luismiguelpazosmoreno <lmpazos77@gmail.com>
2019-11-15 12:19:32 +0100

    Cambios en el código para que el número sea entre 1 y 50

--- luisAdivino/src/Aciertos.java
+++ luisAdivino/src/Aciertos.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Aciertos {
 
     public static void main(String[] args) {
+    	
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        //int x = (int) (500 *Math.random());
+        
+        int x = (int) (Math.random()*(50-1)+1);
+        
         //System.out.print(x);
+        
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("\nIntroduce el valor de X: ");
